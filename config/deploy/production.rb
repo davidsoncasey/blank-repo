56c5757c28a9ff9de98dbe93dd1326c08ed6720b
Casey Davidson <casey@planetargon.com>
2016-02-09 15:37:51 -0800

    Add more verbose messaging to Capistrano configuration files.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,2 @@
-puts "Production deploy"
+branch_name = /On branch ([\w\d\-_]+)\n/.match(`git status`)[1]
+puts "Ran deploy for site production and branch #{ branch_name }"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,2 @@
-puts "Staging deploy"
+branch_name = /On branch ([\w\d\-_]+)\n/.match(`git status`)[1]
+puts "Ran deploy for site staging and branch #{ branch_name }"
